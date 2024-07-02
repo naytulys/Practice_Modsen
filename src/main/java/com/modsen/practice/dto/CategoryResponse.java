0c8e2fafa9d6c62cfdea5dd6b9f98ce6659f7869
igorek <igor-titov-2004@mail.ru>
2024-07-02 10:53:13 +0300

    add test product service

--- src/main/java/com/modsen/practice/dto/CategoryResponse.java
+++ src/main/java/com/modsen/practice/dto/CategoryResponse.java
@@ -5,6 +5,7 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CategoryResponse {
     private Long id;
     private String name;
--- src/main/java/com/modsen/practice/entity/Category.java
+++ src/main/java/com/modsen/practice/entity/Category.java
@@ -1,10 +1,7 @@
 package com.modsen.practice.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Set;
 
@@ -15,6 +12,7 @@ import java.util.Set;
 
 @Entity
 @Table(name = "categories")
+@Builder
 public class Category {
 
     @Id
